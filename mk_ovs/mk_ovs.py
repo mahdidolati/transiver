8da91cb69ecaa4b19cfdca535ce02f01d0c0a475
mahdidolati <mehdidolati69@gmail.com>
2015-06-26 07:52:14 -0600

    run ctrl on remote

--- mk_ovs/mk_ovs.py
+++ mk_ovs/mk_ovs.py
@@ -1,10 +1,14 @@
 import subprocess
 import sys
 
-s_mode = False
+# w for switch, s for server and c for controller
+s_mode = "-w"
 if "-s" == sys.argv[1]:
-  s_mode = True
+  s_mode = sys.argv[1]
   s_ip = sys.argv[2]
+elif "-c" == sys.argv[1]:
+  s_mode = sys.argv[1]
+  c_ip = sys.argv[2]
 
 cmd = "ifconfig"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
@@ -16,15 +20,16 @@ for intf in eth_list:
     continue
   eth_num = intf[0]
   print eth_num
-  if int(eth_num) == 0:
-    continue
   eth_ip = intf.split("inet addr:")[1].split("  Bcast")[0]
   print eth_ip
-  if eth_ip[3] == '1' and s_mode == False:
+  if int(eth_num) == 0 and s_mode == "-c":
+    cmd = "sudo ~/pox/pox.py forwarding.l2_learning &"
+    continue
+  if eth_ip[3] == '1' and s_mode != "-s":
     cmd = "sudo sh ~/transiver/mk_ovs/mk_ovs.sh %s %s" %(eth_num,eth_ip)
     print cmd
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-  if eth_ip[3] == '1' and s_mode == True:
+  if eth_ip[3] == '1' and s_mode == "-s":
     cmd = "sudo ifconfig eth%s %s netmask 255.255.255.0" %(eth_num, s_ip)
     subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 
