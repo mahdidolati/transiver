e2365fc3a10e07625156096a3e792e5ee5c77fa3
mahdidolati <mehdidolati69@gmail.com>
2015-06-26 16:33:47 -0600

    resolve remote ctrl ip

--- mk_ovs/mk_ovs.py
+++ mk_ovs/mk_ovs.py
@@ -8,7 +8,8 @@ if "-s" == sys.argv[1]:
   s_ip = sys.argv[2]
 elif "-c" == sys.argv[1]:
   s_mode = sys.argv[1]
-  c_ip = sys.argv[2]
+  # currently is hardcoded, but make it dynamic
+  #c_ip = sys.argv[2]
 
 cmd = "ifconfig"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
@@ -24,9 +25,20 @@ for intf in eth_list:
   print eth_ip
   if int(eth_num) == 0 and s_mode == "-c":
     cmd = "sudo ~/pox/pox.py forwarding.l2_learning &"
+    subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    fw = open("ctrl_ip", "w")
+    fw.write(eth_ip)
+    fw.close()
     continue
   if eth_ip[3] == '1' and s_mode != "-s":
-    cmd = "sudo sh ~/transiver/mk_ovs/mk_ovs.sh %s %s" %(eth_num,eth_ip)
+    fr = open("ctrl_ip", "r")
+    while True:
+      ctrl_ip = fr.read()
+      if ctrl_ip != "":
+        break
+      fr.seek(0,0)
+    # end of while
+    cmd = "sudo sh ~/transiver/mk_ovs/mk_ovs.sh %s %s %s" %(eth_num,eth_ip,ctrl_ip)
     print cmd
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
   if eth_ip[3] == '1' and s_mode == "-s":
--- mk_ovs/mk_ovs.sh
+++ mk_ovs/mk_ovs.sh
@@ -6,7 +6,7 @@ addPort() {
 ovs-vsctl -- --may-exist add-br br0
 addPort $1
 
-ovs-vsctl set-controller br0 tcp:155.98.39.40:6633
+ovs-vsctl set-controller br0 tcp:$3:6633
 ovs-vsctl set-fail-mode br0 secure
 
 # ifconfig br0 $2 netmask 255.255.255.0
