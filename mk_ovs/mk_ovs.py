8016aca7721da43a4b967389f80c0b83d1bde459
mahdidolati <mehdidolati69@gmail.com>
2015-06-27 08:26:34 -0600

    run pox in backgroound

--- mk_ovs/mk_ovs.py
+++ mk_ovs/mk_ovs.py
@@ -1,6 +1,21 @@
 import subprocess
 import sys
+import logging
+import threading
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('log.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
+class runPox(threading.Thread):
+  def run(self):
+    cmd = "sudo sh run_pox.sh &"
+    p = subprocess.Popen(cmd, shell=True)
+    logger.info('ran pox')
+    
 # w for switch, s for server and c for controller
 s_mode = "-w"
 if "-s" == sys.argv[1]:
@@ -24,13 +39,21 @@ for intf in eth_list:
   eth_ip = intf.split("inet addr:")[1].split("  Bcast")[0]
   print eth_ip
   if int(eth_num) == 0 and s_mode == "-c":
-    cmd = "sudo ~/pox/pox.py forwarding.l2_learning &"
-    subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    logger.info('in controller')
+    # cmd = "sudo ~/pox/pox.py forwarding.l2_learning &"
+    # p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    # logger.info('ran pox')
+    # (output, err) = p.communicate()
+    # logger.info("run pox out %s\nrun pox err %s" %(output,err))
+    runPoxInst = runPox()
+    runPoxInst.start()
     fw = open("ctrl_ip", "w")
     fw.write(eth_ip)
     fw.close()
+    logger.info('ctrl ip is %s\nend of controller' %eth_ip)
     continue
   if eth_ip[3] == '1' and s_mode != "-s":
+    logger.info('in switch')
     fr = open("ctrl_ip", "r")
     while True:
       ctrl_ip = fr.read()
@@ -44,9 +67,11 @@ for intf in eth_list:
       fr.close()
       fr = open("ctrl_ip", "r")
     # end of while
+    logger.info('remote ip is %s' %ctrl_ip)
     cmd = "sudo sh ~/transiver/mk_ovs/mk_ovs.sh %s %s %s" %(eth_num,eth_ip,ctrl_ip)
-    print cmd
+    logger.info(cmd)
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    logger.info('end of switch')
   if eth_ip[3] == '1' and s_mode == "-s":
     cmd = "sudo ifconfig eth%s %s netmask 255.255.255.0" %(eth_num, s_ip)
     subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
