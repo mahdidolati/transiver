c94fbdea23d7abcbbab25ba3f175fe740acadd5c
mahdidolati <mehdidolati69@gmail.com>
2015-06-27 10:59:30 -0600

    add logging and solve problem of relative path

--- mk_ovs/mk_ovs.py
+++ mk_ovs/mk_ovs.py
@@ -3,9 +3,15 @@ import sys
 import logging
 import threading
 #
+PATH_TO_FILE = "/users/mdolati/transiver/mk_ovs/"
+LOG_FILE = "log.log"
+CTRL_IP_FILE = "ctrl_ip"
+LOG_PATH = PATH_TO_FILE + LOG_FILE
+CTRL_IP_PATH = PATH_TO_FILE + CTRL_IP_FILE
+#
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler('log.log')
+handler = logging.FileHandler(LOG_PATH)
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
@@ -13,16 +19,28 @@ logger.addHandler(handler)
 #
 class runPox(threading.Thread):
   def run(self):
-    cmd = "sudo sh run_pox.sh &"
+    cmd = "sudo netstat -lnp | grep 6633"
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    logger.info("is another ctrl running %s" %output)
+    if len(output) > 4:
+      l1 = output.split("/")
+      if len(l1) > 0:
+        l2 = l1[0].split(" ")
+        pid = l2[len(l2)-1]
+        cmd = "sudo kill -9 %s" %pid
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        logger.info("killed %s" %pid)
+      #endIf,if
+    cmd = "sudo sh ~/transiver/mk_ovs/run_pox.sh &"
     p = subprocess.Popen(cmd, shell=True)
     logger.info('ran pox')   
 # w for switch, s for server and c for controller
-s_mode = "-w"
-if "-s" == sys.argv[1]:
-  s_mode = sys.argv[1]
+s_mode = sys.argv[1]
+if "-s" == s_mode:
   s_ip = sys.argv[2]
-elif "-c" == sys.argv[1]:
-  s_mode = sys.argv[1]
+#
+logger.info("starting mk_ovs for a %s" %s_mode)
 #
 cmd = "ifconfig"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
@@ -31,7 +49,7 @@ output.replace("Ethernet", " ")
 eth_list = output.split("eth")
 # obtain ctrl ip
 if s_mode == "-w":
- fr = open("ctrl_ip", "r")
+ fr = open(CTRL_IP_PATH, "r")
  while True:
    ctrl_ip = fr.read()
    if len(ctrl_net) < 5:
@@ -40,8 +58,10 @@ if s_mode == "-w":
      continue
    else:
      ctrl_net = ctrl_ip.split(".")[2]
+     logger.info("ctrl ip is %s beg" %ctrl_ip)
      break
 #endIf,while,if
+#
 isPoxRunning = False
 for intf in eth_list:
   if len(intf) == 0:
@@ -53,7 +73,7 @@ for intf in eth_list:
   logger.info("observing eth%s with ip %s" %(eth_num, eth_ip))
   if s_mode == "-c":
     logger.info('in controller')         
-    fw = open("ctrl_ip", "w")
+    fw = open(CTRL_IP_PATH, "w")
     fw.write(eth_ip)
     fw.close()
     logger.info('ctrl ip is %s\nend of controller' %eth_ip)
@@ -67,6 +87,7 @@ for intf in eth_list:
     logger.info('in switch')
     eth_net = eth_ip.split(".")[2]
     if eth_net == ctrl_net:
+      logger.info("same net as ctrl %s with eth%s" %(eth_net,eth_num))
       continue
     logger.info('remote ip is %s' %ctrl_ip)
     cmd = "sudo sh ~/transiver/mk_ovs/mk_ovs.sh %s %s %s" %(eth_num,eth_ip,ctrl_ip)
---
+++ mk_ovs/run_pox.sh
@@ -0,0 +1,1 @@
+sudo ~/pox/pox.py forwarding.l2_learning &
