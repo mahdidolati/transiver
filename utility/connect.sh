72855e5977e2a4e161d6ce916281a22285119cd7
mahdidolati <mehdidolati69@gmail.com>
2015-07-18 13:04:10 -0600

    runall  from one host

---
+++ utility/connect.sh
@@ -0,0 +1,7 @@
+ssh mdolati@servers-1.TreeTopo.RSDN.emulab.net ifconfig;
+ssh mdolati@servers-2.TreeTopo.RSDN.emulab.net ifconfig;
+ssh mdolati@servers-3.TreeTopo.RSDN.emulab.net ifconfig;
+ssh mdolati@servers-4.TreeTopo.RSDN.emulab.net ifconfig;
+ssh mdolati@servers-5.TreeTopo.RSDN.emulab.net ifconfig;
+ssh mdolati@servers-6.TreeTopo.RSDN.emulab.net ifconfig;
+ssh mdolati@servers-7.TreeTopo.RSDN.emulab.net ifconfig;
